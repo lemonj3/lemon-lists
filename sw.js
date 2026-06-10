6be4372bc25615ea129231f0755ded365f6bd78d
lemonj3 <heyant3@gmail.com>
2026-06-10 16:54:56 +0900

    first upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE = 'my-lists-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './icon.svg',
+  './manifest.json',
+  'https://fonts.googleapis.com/css2?family=Gaegu:wght@400;700&display=swap',
+  'https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css',
+  'https://unpkg.com/lucide@latest/dist/umd/lucide.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS.map(u => new Request(u, {mode: 'no-cors'}))))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
+  );
+});
